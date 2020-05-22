e4180427b55912344f7efcf672abb88151be6006
Victor Yoalli <victor@vientodigital.com>
2020-05-21 21:26:30 -0500

    minor fixes

---
+++ resources/views/livewire/no_more_tries.blade.php
@@ -0,0 +1,4 @@
+ <div style="margin-top: 1em;border:1px solid;border-color:#d2d6dd;border-radius:0.25rem;">
+    <div style="font-size: 3rem;text-align: center;font-weight: 500;">{{$survey->name}}</div>
+    <div style="text-align: center;">Ya has agotado los intentos para responder esta encuesta</div>
+</div>
--- src/Models/Survey.php
+++ src/Models/Survey.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Survey extends Model
 {
     protected $appends = ['limit_entries_participant'];
+
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
@@ -22,7 +23,8 @@ class Survey extends Model
         return $this->hasMany(Section::class);
     }
 
-    public function hasSections(){
+    public function hasSections()
+    {
         return $this->sections()->exists();
     }
 
@@ -30,47 +32,61 @@ class Survey extends Model
     {
         return $this->hasMany(Question::class);
     }
-    
 
     public function entries()
     {
         return $this->hasMany(Entry::class);
     }
+
     public function withoutSection()
     {
         return $this->questions()->withoutSection();
     }
-    public function limitExceeded($user_id){
-        if($this->limit_entries_per_participant==0)return false;
-        $entry = Entry::where('user_id',$user_id)->where('survey_id',$this->id)->whereNotNull('completed_at')->get();
-        if($entry->count()>=$this->limit_entries_per_participant)return true;
+
+    public function limitExceeded($user_id)
+    {
+        if ($this->limit_entries_per_participant == 0) {
+            return false;
+        }
+        $entry = Entry::where('user_id', $user_id)->where('survey_id', $this->id)->whereNotNull('completed_at')->get();
+        if ($entry->count() >= $this->limit_entries_per_participant) {
+            return true;
+        }
         return false;
     }
 
-    public function questionsNotAnswered(Entry $entry=null){
+    public function questionsNotAnswered(Entry $entry = null)
+    {
         return $this->questions()->notAnswered($entry);
     }
-    
 
-    public function getLimitEntriesPerParticipantAttribute(){
-        return $this->settings==null?0:$this->settings->get('limit_entries_per_participant')??0;
-    }
-
-    public function setLimitEntriesPerParticipantAttribute($value){
-         $this->settings = collect($this->settings)->put('limit_entries_per_participant',$value);
-    }
-    public function getGroupQuestionsPerSectionAttribute():bool{
-        return $this->settings->get('group_questions_per_section')??false;
+    public function getLimitEntriesPerParticipantAttribute()
+    {
+        return $this->settings == null ? 0 : $this->settings->get('limit_entries_per_participant') ?? 0;
     }
 
-    public function setGroupQuestionsPerSectionAttribute($value){
-         $this->settings = collect($this->settings)->put('group_questions_per_section',$value);
-    }
-    public function getQuestionsPerPageAttribute(){
-        return $this->settings->get('questions_per_page')??1;
+    public function setLimitEntriesPerParticipantAttribute($value)
+    {
+        $this->settings = collect($this->settings)->put('limit_entries_per_participant', $value);
     }
 
-    public function setQuestionsPerPageAttribute($value){
-         $this->settings = collect($this->settings)->put('questions_per_page',$value);
+    public function getGroupQuestionsPerSectionAttribute():bool
+    {
+        return $this->settings->get('group_questions_per_section') ?? false;
+    }
+
+    public function setGroupQuestionsPerSectionAttribute($value)
+    {
+        $this->settings = collect($this->settings)->put('group_questions_per_section', $value);
+    }
+
+    public function getQuestionsPerPageAttribute()
+    {
+        return $this->settings->get('questions_per_page') ?? 1;
+    }
+
+    public function setQuestionsPerPageAttribute($value)
+    {
+        $this->settings = collect($this->settings)->put('questions_per_page', $value);
     }
 }
